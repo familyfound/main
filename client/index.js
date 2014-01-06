21b5c60aa41fc68a89e78da972c6b5a50d311f83
Jared Forsyth <jared@jaredforsyth.com>
2014-01-06 15:45:39 -0700

    small api change

--- client/index.js
+++ client/index.js
@@ -1,25 +1,28 @@
 
 var LoginPage = require('login-page')
 
-React.renderComponent(LoginPage({checkPath: '/auth/check-login'})
-
 var App = React.createClass({
   getInitialState: function () {
     return {
       token: null
     }
   },
-  gotToken: function (token) {
-    this.setState({token: token})
+  authorized: function (token, data) {
+    this.setState({
+      token: token,
+      userData: data
+    })
   },
   render: function () {
     if (!this.state.token) {
       return LoginPage({
         checkPage: this.props.checkPage,
-        gotToken: this.gotToken
+        authorized: this.authorized
       })
     }
     return View({token: this.state.token})
   }
 })
+
+React.renderComponent(LoginPage({checkPath: '/auth/check-login'})
 
