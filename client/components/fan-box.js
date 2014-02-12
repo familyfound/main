b3e91e85bef85589b6edb7f9d02aa5dbbc389a4c
Jared Forsyth <jared@jaredforsyth.com>
2014-02-12 11:46:23 -0700

    widthing works

--- client/components/fan-box.js
+++ client/components/fan-box.js
@@ -2,6 +2,7 @@
 var d = React.DOM
   , Fan = require('fan')
   , classes = require('./classes')
+  , Tip = require('tip')
 
 function nodeClasses(data) {
   if (!data.rels) return {path: 'not-loaded'};
@@ -23,20 +24,43 @@ function nodeTip(data) {
 }
 
 var FanBox = module.exports = React.createClass({
+  getDefaultProps: function () {
+    return {
+      sweep: Math.PI*4/3,
+      width: 500,
+      gens: 6,
+      padding: 5
+    }
+  },
+  getHeight: function () {
+    var r = this.props.width / 2 - this.props.padding
+      , iw = r / this.props.gens
+      , a = this.props.sweep / 2 - Math.PI / 2
+    return r + r * Math.sin(a) + this.props.padding * 2
+  },
   render: function () {
+    var height = this.getHeight()
+      , r = this.props.width / 2
+      , transform = 'translate(' + this.props.width/2 + ', ' + r + ')'
     return d.svg({
       className: 'fan-box show-completion',
-      width: 300,
-      height: 230
+      width: this.props.width,
+      height: height
     }, Fan({
       attr: 'rels',
       getClasses: nodeClasses,
       hoverTips: nodeTip,
       onClick: this.props.onClick,
       onRightClick: this.props.onRightClick,
-      transform: 'translate(150,150)',
+      transform: transform,
       manager: this.props.manager,
-      id: this.props.pid
+      id: this.props.pid,
+      options: {
+        width: r / this.props.gens,
+        doubleWidth: false,
+        sweep: this.props.sweep,
+        offset: 0,
+      }
     }))
   }
 })
